b229bd00133f35af901359062b731ba74b5fde1b
Воронин Игорь Вячеславович <igor.voronin@mts.ru>
2021-11-24 18:16:07 +0300

    C4 toolbox

--- Editor/js/modules/toolbox.js
+++ Editor/js/modules/toolbox.js
@@ -1,33 +1,62 @@
 export class toolbox_controller
 {
     div;
-
     create_item_div( item)
     {
-        let name_div = document.createElement('div');
-        name_div.innerHTML = `${item.name}`;
-        name_div.className = "toolbox_item";
-        return name_div;
+        let item_div = document.createElement('div');
+        item_div.style.width = "auto"
+        item_div.style.cursor = "";
+       
+        item_div.innerHTML = `<div style="display: inline-block; width: 20;"></div><img style="vertical-align: middle;" src="${item.image}"><div style="display:  inline-block;">${item.name}</div>`;
+        //name_div.className = "toolbox_item";
+
+        //add selection behavior
+        item_div.onmouseover = (e) => { item_div.style.backgroundColor  = 'lightgray'; }
+        item_div.onmouseout = (e) => { item_div.style.backgroundColor  = 'white'; }
+        return item_div;
     }
     create_item_group_div( group )
     {
-        let div = document.createElement("div");
-        div.className = "toolbox_group";
+        let group_div = document.createElement("div");
+        group_div.style.width = "100%";
+        //div.className = "toolbox_group";
         let name_div = document.createElement('div');
-        name_div.className = "toolbox_group_name";
-        name_div.innerHTML = `${group.name}`;
-        div.appendChild( name_div );
+        name_div.style.width = "auto";
+        name_div.style.cursor = "pointer";
+
+        //name_div.className = "toolbox_group_name";
+        name_div.innerHTML = `<img style="vertical-align: middle;display: inline-block;" src=\"../../Editor/image/toolbox/expanded.png"><div style="display: inline-block;">${group.name}</div>`;
+        let group_img = name_div.firstElementChild;
+        group_div.appendChild( name_div );
+        let items = [];
 
         for( const i of group.items){
-            div.appendChild( this.create_item_div( i ) );
+            let item_div = this.create_item_div( i );
+            group_div.appendChild( item_div );
+            items.push ( item_div );
         }
+        group_div.collapsed = false;
+
+        name_div.onclick = (e) =>{
+            items.forEach( (item) =>{
+                item.hidden = !group_div.collapsed;
+            })
+            group_div.collapsed = !group_div.collapsed;
+            if(group_div.collapsed ){
+                group_img.src = "../../Editor/image/toolbox/collapsed.png";
+            } else{
+                group_img.src = "../../Editor/image/toolbox/expanded.png";
+            }
+        };
         //alert( div.outerHTML );
-        return div;
+        return group_div;
     }
 
     constructor( div, groups )
     {
         this.div = div;
+        div.style.cursor = "default";
+        div.style.userSelect = "none";
         for( const g of groups)
         {
             div.appendChild(this.create_item_group_div( g ));
@@ -50,8 +79,10 @@ export class toolbox_group
 
 export class toolbox_item{
     name;
-    constructor( name )
+    image;
+    constructor( name, image )
     {
         this.name = name;
+        this.image= image;
     }
 }
